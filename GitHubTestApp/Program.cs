3caa9cd166a6db69980b7b8ad13e814c903d87d5
Husni Hamdan <husnihhamdan@gmail.com>
2024-03-27 16:07:56 +0100

    #1 test-branch is created

--- GitHubTestApp/Program.cs
+++ GitHubTestApp/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
 
 //This line is the first code od the project
 Console.WriteLine("this is first line");
+
+//This line is the added from test-branch
+Console.WriteLine("This is added from test-branch");
