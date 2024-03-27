ed5dfbb8b6e6097ee3ba342409f8b0fd2d47eef6
husni <husni@HUSSNI>
2024-03-27 15:33:15 +0100

    Add project files.

---
+++ GitHubTestApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//This line is the first code od the project
+Console.WriteLine("this is first line");
