776767ef551f8cc7b32028103cdf989731775339
Husni Hamdan <husnihhamdan@gmail.com>
2024-03-27 16:24:11 +0100

    new code added from test2-Newbranch

--- GitHubTestApp/Program.cs
+++ GitHubTestApp/Program.cs
@@ -8,3 +8,8 @@ Console.WriteLine("this is first line");
 
 //This line is the added from test-branch
 Console.WriteLine("This is added from test-branch");
+
+//This line is the added from test2-Newbranch
+Console.WriteLine("This is added from test2-Newbranch");
+
+
